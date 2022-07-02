c9d2e73ce730bbb05ab60d079d3f7fb96182278b
matches <matches@Matches-2.local>
2022-07-02 11:27:25 +0100

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,20 @@
+import '../styles/globals.css'
+import { AppProps } from 'next/app'
+import { Provider } from 'react-redux';       
+import store from '../redux/store';       
+
+import { ApolloProvider } from "@apollo/client";
+import client from "../apollo-client";
+
+
+function MyApp({ Component, pageProps }) {
+  return (
+      <ApolloProvider client={client}>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </ApolloProvider>
+  );
+}
+
+export default MyApp
